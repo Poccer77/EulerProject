46547f0042ec62713f61716948614ef10cb5c0d9
Poccer77 <doepfner.noah@gmail.com>
2023-04-09 17:23:19 +0200

    4 WORKS BEBI (yes I googled, yes I am ashamed)

--- 3. LargestPrimeFactor.rkt
+++ 3. LargestPrimeFactor.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname LargestPrimeFactor) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname |3. LargestPrimeFactor|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;;largest prime factors
 
 (define (largest-prime-factor x)
@@ -10,15 +10,13 @@
          [(and (= (modulo x factor) 0) (not (or (= factor 1) (= factor x)))) #f]
          [(> factor (/ x 2)) #t]
          [else (prime? x (+ factor 1))]))
-         
-        
     (define (factorization x factor accu)
        (cond
          [(<= factor 1) factor]
          [(and (= (modulo x factor) 0) (prime? factor 1)) factor]
-         [else (factorization x (if (integer? (/ x accu)) (/ x accu) (round (/ x accu))) (+ accu 1))]))]
+         [else (factorization x (round (/ x accu)) (+ accu 1))]))]
     
-    (factorization x (if (integer? (/ x 2)) (/ x 2) (round (/ x 2))) 2)))
+    (factorization x (round (/ x 2)) 2)))
 
 (largest-prime-factor 600851475143)
            
--- 4. LargestPalyndromicFactor.rkt
+++ 4. LargestPalyndromicFactor.rkt
@@ -1,20 +1,19 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname LargestPalyndromicFactor) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname |4. LargestPalyndromicFactor|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;; largest palyndrome of two numbers
 
 (define (largest-palyndrome digits)
   (local
     
    [(define largest-multiplicant (string->number (make-string digits #\9)))
-    (define (palyndrome? str)
-       (string=? (substring str 0 (floor (/ (string-length str) 2))) (list->string (reverse (string->list (substring str (ceiling (/ (string-length str) 2)) (string-length str)))))))
-    (define (check-through-multiples current-lowest x)
+    (define (palyndrome? str) (string=? (substring str 0 (floor (/ (string-length str) 2))) (list->string (reverse (string->list (substring str (ceiling (/ (string-length str) 2)) (string-length str)))))))
+    (define (check-through-multiples distance-from-highest balance)
        (cond
-         [(palyndrome? (number->string (* current-lowest x))) (list (* current-lowest x) current-lowest x)]
-         [(equal? x largest-multiplicant) (check-through-multiples (- current-lowest 1) (- current-lowest 1))]
-         [else (check-through-multiples current-lowest (+ x 1))]))]
+         [(palyndrome? (number->string (* (- largest-multiplicant balance) (- largest-multiplicant (- distance-from-highest balance))))) (* (- largest-multiplicant balance) (- largest-multiplicant (- distance-from-highest balance)))]
+         [(>= balance distance-from-highest) (check-through-multiples (+ distance-from-highest 1) 0)]
+         [else (check-through-multiples distance-from-highest (+ balance 1))]))]
 
-    (check-through-multiples largest-multiplicant largest-multiplicant)))
+    (check-through-multiples 0 0)))
 
 (largest-palyndrome 3) 
